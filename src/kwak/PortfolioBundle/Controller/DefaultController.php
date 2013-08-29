7d7547ee5fd830dfa58cbf7d4468d2e56bbdc19d
Ron Cohen <ronihcohen@gmail.com>
2013-08-29 16:03:43 +0300

    Initial commit

---
+++ src/kwak/PortfolioBundle/Controller/DefaultController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace kwak\PortfolioBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use kwak\PortfolioBundle\Entity\imgSets;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+   	$product = new imgSets();
+    $product->setName('A Foo Bar');
+    $product->setTitle('lalala');
+
+
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($product);
+    $em->flush();
+
+    return new Response('Created product id '.$product->getId());
+    }
+
+    public function showAction($id)
+	{
+    $product = $this->getDoctrine()
+        ->getRepository('kwakPortfolioBundle:imgSets')
+        ->find($id);
+
+    if (!$product) {
+        throw $this->createNotFoundException(
+            'No product found for id '.$id
+        );
+    }
+    return $this->render(
+        'kwakPortfolioBundle:Default:base.html.twig',
+        array('name' => $product->getName())
+    );
+	}
+}
---
+++ src/kwak/PortfolioBundle/kwakPortfolioBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace kwak\PortfolioBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class kwakPortfolioBundle extends Bundle
+{
+}
